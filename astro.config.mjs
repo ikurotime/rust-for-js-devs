372bcff86335964f013927d94f1fe3c7f4470e7f
David Huertas <davidhuertasortiz@gmail.com>
2024-05-17 22:44:36 +0200

    refactor: Add Vercel adapter to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  output: 'server'
+  output: 'server',
+  adapter: vercel()
 });
