05b8761c2f02ae34d2cf8952707676067967c109
David <davidhuertasortiz@gmail.com>
2025-07-11 01:39:20 +0200

    Enhance localization support and update documentation structure
    - Added support for multiple languages in `astro.config.mjs`, including English and Spanish locales.
    - Updated the `Banner.astro` component to display localized messages based on the selected language.
    - Removed obsolete documentation files and added new content covering essential Rust concepts, including a guessing game and practical projects.
    - Improved the overall organization of documentation files for better navigation and user experience.
    - Updated the favicon and logo assets for enhanced branding consistency.

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,13 +9,21 @@ export default defineConfig({
       plugins: [starlightThemeNova()],
       title: 'Rust For JS Devs',
       description: 'Learn Rust concepts mapped to your JavaScript knowledge',
+      defaultLocale: 'en',
+      locales: {
+        en: {
+          label: 'English'
+        },
+        es: {
+          label: 'Español'
+        }
+      },
       components: {
-        // Header: './src/components/Header.astro'
+        Header: './src/components/Header.astro',
         Banner: './src/components/Banner.astro',
-        Pagination: './src/components/Pagination.astro',
+        // Pagination: './src/components/Pagination.astro',
         Footer: './src/components/Footer.astro'
       },
-      lastUpdated: true,
       pagination: true,
       editLink: {
         baseUrl: 'https://github.com/ikurotime/rust-for-js-devs/edit/main/'
@@ -29,24 +37,101 @@ export default defineConfig({
       ],
       sidebar: [
         {
-          label: 'Getting Started',
-          link: '/getting-started/'
+          label: 'Welcome',
+          link: '/welcome/'
         },
         {
           label: 'What Can I Do with Rust?',
           slug: 'what-can-i-do-with-rust'
         },
         {
-          label: 'Guides',
+          label: 'Introduction',
           items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: 'guides/example' }
+            // { label: 'Getting Started', slug: 'introduction/getting-started' },
+            { label: 'Installation', slug: 'introduction/installation' },
+            { label: 'Hello World', slug: 'introduction/hello-world' },
+            { label: 'Hello Cargo', slug: 'introduction/hello-cargo' }
+          ]
+        },
+
+        {
+          label: 'Common Concepts',
+          items: [
+            {
+              label: 'Variables and Mutability',
+              slug: 'common-concepts/variables-mutability'
+            },
+            { label: 'Data Types', slug: 'common-concepts/data-types' },
+            { label: 'Functions', slug: 'common-concepts/functions' },
+            { label: 'Comments', slug: 'common-concepts/comments' },
+            { label: 'Control Flow', slug: 'common-concepts/control-flow' },
+            { label: 'Strings', slug: 'common-concepts/strings' }
           ]
         },
         {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' }
+          label: 'Ownership & Borrowing',
+          items: [
+            {
+              label: 'What is Ownership',
+              slug: 'ownership-borrowing/what-is-ownership'
+            },
+            {
+              label: 'Borrowing in Depth',
+              slug: 'ownership-borrowing/borrowing-in-depth'
+            },
+            { label: 'Lifetimes', slug: 'ownership-borrowing/lifetimes' }
+          ]
+        },
+        {
+          label: 'Structs & Enums',
+          items: [
+            {
+              label: 'Defining Structs',
+              slug: 'structs-enums/defining-structs'
+            },
+            { label: 'Struct Example', slug: 'structs-enums/struct-example' },
+            { label: 'Methods', slug: 'structs-enums/methods' },
+            { label: 'Defining Enums', slug: 'structs-enums/defining-enums' },
+            { label: 'Match', slug: 'structs-enums/match' },
+            { label: 'If Let', slug: 'structs-enums/if-let' }
+          ]
+        },
+        {
+          label: 'Collections',
+          items: [
+            { label: 'Vectors', slug: 'collections/vectors' },
+            { label: 'Strings', slug: 'collections/strings' },
+            { label: 'Hash Maps', slug: 'collections/hash-maps' }
+          ]
+        },
+        {
+          label: 'Error Handling',
+          items: [{ label: 'Error Handling Overview', slug: 'error-handling' }]
+        },
+        {
+          label: 'Practical Projects',
+          items: [
+            {
+              label: 'Guessing Game',
+              slug: 'guessing-game'
+            },
+            {
+              label: 'Simple Web Server',
+              slug: 'practical-projects/simple-web-server'
+            }
+          ]
         }
+        // {
+        //   label: 'Guides',
+        //   items: [
+        //     // Each item here is one entry in the navigation menu.
+        //     { label: 'Example Guide', link: 'guides/example' }
+        //   ]
+        // }
+        // {
+        //   label: 'Reference',
+        //   autogenerate: { directory: 'reference' }
+        // }
       ]
     })
   ]
